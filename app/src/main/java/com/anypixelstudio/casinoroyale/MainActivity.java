a93612de7804615137d1b079dafb0a2da56a4dd5
Sm1the <apxahreji123@gmail.com>
2020-02-15 18:17:35 +0700

    Add assets(plus,minus,plus_moneu,setting,table)

--- app/src/main/java/com/anypixelstudio/casinoroyale/MainActivity.java
+++ app/src/main/java/com/anypixelstudio/casinoroyale/MainActivity.java
@@ -2,12 +2,53 @@ package com.anypixelstudio.casinoroyale;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView spinWheel;
+    Random random;
+    int degree = 0, degree_old = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        random = new Random();
+        spinWheel = findViewById(R.id.iv_SpinWheel);
+
+    }
+
+    public void startSpin(View view){
+        degree_old = degree % 360;
+        degree = random.nextInt(3600)+720;
+        RotateAnimation rotateAnimation = new RotateAnimation(degree_old,degree,RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(3600);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setInterpolator(new DecelerateInterpolator());
+        rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        spinWheel.startAnimation(rotateAnimation);
     }
 }
